dc2d79541d2ced2ef9bdfe84fac11361de034fa2
mattiasinokuchi <8-b@tuta.io>
2021-03-23 15:29:05 +0100

    changed query

--- api/news.js
+++ api/news.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 export default async function (req, res) {
-  let response = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + process.env.NEWSAPI);
+  let response = await fetch("https://newsapi.org/v2/top-headlines?language=en&apiKey=" + process.env.NEWSAPI);
   let object = await response.json();
   console.log(object.articles[0]);
   res.json(object.articles[0]);
