27a534ebefc4a8dfbee25198c977b8d63b07a8de
mattiasinokuchi <8-b@tuta.io>
2021-03-23 14:40:37 +0100

    changed markup and fetch API

--- api/news.js
+++ api/news.js
@@ -3,7 +3,8 @@ const fetch = require('node-fetch');
 module.exports = async (req, res) => {
   let response = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + process.env.NEWSAPI);
   let object = await response.json();
-  console.log(object);
-  const date = new Date().toString();
-  res.status(200).send(date);
+  console.log(object.articles[0]);
+  res.json(object.articles[0]);
+  //const date = new Date().toString();
+  //res.status(200).send(date);
 };
